e1f637cda31819f738b458a5899ae354afe29241
Alex Antonov <>
2022-11-09 15:47:06 +0200

    props navigate removed

--- screens/Landing.tsx
+++ screens/Landing.tsx
@@ -1,14 +1,10 @@
 import React from "react";
 import {StyleSheet, View} from "react-native";
-import {RootStackParamList} from "./RootStackParamList";
-import {NativeStackScreenProps} from "@react-navigation/native-stack";
 
 import { CATEGORIES } from "../data/dummy-data";
 import Categories from "../components/categories/Categories";
 
-type Props = NativeStackScreenProps<RootStackParamList, 'Category'>;
-
-const Landing: React.FC<{navigation: Props['navigation']}> = (props) => {
+const Landing: React.FC = () => {
 
   return (
     <View style={styles.page}>
