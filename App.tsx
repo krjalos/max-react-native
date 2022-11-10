f48903166192f59795522435f73841c2a7084a61
Alex Antonov <>
2022-11-10 23:01:29 +0200

    default screen options

--- App.tsx
+++ App.tsx
@@ -8,24 +8,25 @@ import Category from "./screens/Category";
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export default function App() {
-
-  const screenOptions = {
-    title:"All Categories",
-    headerStyle: {
-      backgroundColor: "#351401"
-    },
-    headerTintColor: "#fff",
-    contentStyle: {
-      backgroundColor: "#3f2f25"
-    }
-  };
   
   return (
     <>
       <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="Landing" component={Landing} options={screenOptions}/>
-          <Stack.Screen name="Category" component={Category} options={{...screenOptions, title: "Meals"}}/>
+        <Stack.Navigator screenOptions={{
+          headerStyle: {
+            backgroundColor: "#351401"
+          },
+          headerTintColor: "#fff",
+          contentStyle: {
+            backgroundColor: "#3f2f25"
+          }
+        }}>
+          <Stack.Screen name="Landing" component={Landing} options={{
+            title: "All Categories"
+          }}/>
+          <Stack.Screen name="Category" component={Category} options={{
+            title: "Meals"
+          }}/>
         </Stack.Navigator>
       </NavigationContainer>
     </>
