94c333fc0c687c813a9f4fb223c6e8ac8567a7bb
Alex Antonov <>
2022-11-13 15:14:30 +0200

    drawer setup with example options

--- App.js
+++ App.js
@@ -1,5 +1,25 @@
-import { Text } from 'react-native';
+import {NavigationContainer} from "@react-navigation/native";
+import {createDrawerNavigator} from "@react-navigation/drawer";
+import WelcomeScreen from "./screens/WelcomeScreen";
+import UserScreen from "./screens/UserScreen";
+
+const Drawer = createDrawerNavigator();
+
 
 export default function App() {
-  return <Text>Todo...</Text>;
+
+  return <NavigationContainer>
+    <Drawer.Navigator screenOptions={{
+      headerStyle: {
+        backgroundColor:"rgb(87,0,0)"
+      },
+      headerTintColor: "#fff"
+    }}>
+      <Drawer.Screen name="Welcome" component={WelcomeScreen} options={{
+        drawerActiveBackgroundColor: "#265624",
+        drawerActiveTintColor: "white"
+      }}/>
+      <Drawer.Screen name="User" component={UserScreen}/>
+    </Drawer.Navigator>
+  </NavigationContainer>;
 }
