99157656146fdab442906199d462f21e9a871520
Alex Antonov <>
2022-11-13 20:38:47 +0200

    drawer options - more examples

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import {NavigationContainer} from "@react-navigation/native";
 import {createDrawerNavigator} from "@react-navigation/drawer";
 import WelcomeScreen from "./screens/WelcomeScreen";
 import UserScreen from "./screens/UserScreen";
+import {Ionicons} from "@expo/vector-icons";
 
 const Drawer = createDrawerNavigator();
 
@@ -13,13 +14,18 @@ export default function App() {
       headerStyle: {
         backgroundColor:"rgb(87,0,0)"
       },
-      headerTintColor: "#fff"
+      headerTintColor: "#fff",
+      drawerActiveBackgroundColor: "#265624",
+      drawerActiveTintColor: "white"
     }}>
       <Drawer.Screen name="Welcome" component={WelcomeScreen} options={{
-        drawerActiveBackgroundColor: "#265624",
-        drawerActiveTintColor: "white"
+        drawerLabel:"Welcome screen",
+        drawerIcon: ({color, size}) => <Ionicons name="home" size={size} color={color}/>
       }}/>
-      <Drawer.Screen name="User" component={UserScreen}/>
+      <Drawer.Screen name="User" component={UserScreen} options={{
+        drawerLabel:"User screen",
+        drawerIcon: ({color, size}) => <Ionicons name="person" size={size} color={color}/>
+      }}/>
     </Drawer.Navigator>
   </NavigationContainer>;
 }
