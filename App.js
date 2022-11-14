3c8a9e1ca673392853be3f3d0c8f757872d24a66
Alex Antonov <>
2022-11-14 17:25:45 +0200

    tabs navigation

--- App.js
+++ App.js
@@ -1,31 +1,28 @@
 import {NavigationContainer} from "@react-navigation/native";
-import {createDrawerNavigator} from "@react-navigation/drawer";
+import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
 import WelcomeScreen from "./screens/WelcomeScreen";
 import UserScreen from "./screens/UserScreen";
 import {Ionicons} from "@expo/vector-icons";
 
-const Drawer = createDrawerNavigator();
+const Tabs = createBottomTabNavigator();
 
 
 export default function App() {
 
   return <NavigationContainer>
-    <Drawer.Navigator screenOptions={{
+    <Tabs.Navigator screenOptions={{
       headerStyle: {
-        backgroundColor:"rgb(87,0,0)"
+        backgroundColor: "rgb(87,0,0)"
       },
       headerTintColor: "#fff",
-      drawerActiveBackgroundColor: "#265624",
-      drawerActiveTintColor: "white"
+      tabBarActiveTintColor: "rgb(87,0,0)"
     }}>
-      <Drawer.Screen name="Welcome" component={WelcomeScreen} options={{
-        drawerLabel:"Welcome screen",
-        drawerIcon: ({color, size}) => <Ionicons name="home" size={size} color={color}/>
+      <Tabs.Screen name="Welcome" component={WelcomeScreen} options={{
+        tabBarIcon: ({color, size}) => <Ionicons name="home" size={size} color={color}/>
       }}/>
-      <Drawer.Screen name="User" component={UserScreen} options={{
-        drawerLabel:"User screen",
-        drawerIcon: ({color, size}) => <Ionicons name="person" size={size} color={color}/>
+      <Tabs.Screen name="User" component={UserScreen} options={{
+        tabBarIcon: ({color, size}) => <Ionicons name="person" size={size} color={color}/>
       }}/>
-    </Drawer.Navigator>
+    </Tabs.Navigator>
   </NavigationContainer>;
 }
