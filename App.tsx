6f5ab20b03f23d74e15326496a748e856becaec1
Alex Antonov <>
2022-11-16 22:02:55 +0200

    nested navigators

--- App.tsx
+++ App.tsx
@@ -1,12 +1,36 @@
+import React from "react";
+
 import {NavigationContainer} from "@react-navigation/native";
 import {createNativeStackNavigator} from "@react-navigation/native-stack";
+import {createDrawerNavigator} from "@react-navigation/drawer";
 import {RootStackParamList} from "./screens/RootStackParamList";
 
 import Landing from "./screens/Landing";
 import Category from "./screens/Category";
 import Meal from "./screens/Meal";
+import Favorites from "./screens/Favorites";
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
+
+const Drawer = createDrawerNavigator();
+
+const DrawerNavigator:React.FC = () => {
+  return (
+    <Drawer.Navigator useLegacyImplementation={true} screenOptions={{
+      headerStyle: {
+        backgroundColor: "#351401"
+      },
+      headerTintColor: "#fff",
+      sceneContainerStyle: {
+        backgroundColor: "#3f2f25"
+      }
+    }}>
+      <Drawer.Screen name="Meals" component={Landing}/>
+      <Drawer.Screen name="Favorites" component={Favorites}/>
+    </Drawer.Navigator>
+  );
+}
+
 
 export default function App() {
 
@@ -22,9 +46,9 @@ export default function App() {
             backgroundColor: "#3f2f25"
           }
         }}>
-          <Stack.Screen name="Landing" component={Landing} options={{
-            title: "All Categories"
-          }}/>
+          <Stack.Screen name="Landing" component={DrawerNavigator} options={{
+            title: "All Categories",
+            headerShown: false}}/>
           <Stack.Screen name="Category" component={Category}/>
           <Stack.Screen name="Meal" component={Meal}/>
         </Stack.Navigator>
---
+++ screens/Favorites.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import {Text} from "react-native";
+
+
+const Favorites: React.FC = () => {
+  return (
+    <Text>Favorites</Text>
+  );
+}
+
+export default Favorites;
